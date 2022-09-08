7e68e8432ad260a634b159d66679ce55558abc99
geonya <geony8410@gmail.com>
2022-09-09 00:21:31 +0900

    main page overflowX hidden

--- components/Main.tsx
+++ components/Main.tsx
@@ -9,6 +9,7 @@ export default function Main({ children }: { children: React.ReactNode }) {
       scrollbarSize={6}
       scrollHideDelay={1000}
       sx={{
+        overflowX: 'hidden',
         width: '100%',
         height: '100%',
         backgroundColor:
