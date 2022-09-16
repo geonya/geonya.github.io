3e2b1d6a9c44da5ba4fcf71ab89f7b5aed2420b3
geonya <geony8410@gmail.com>
2022-09-16 13:09:07 +0900

    deploy

--- hooks/useIsDark.tsx
+++
@@ -1,7 +0,0 @@
-import { useMantineTheme } from '@mantine/core'
-
-export default function useIsDark() {
-  const theme = useMantineTheme()
-  const isDark = theme.colorScheme === 'dark'
-  return isDark
-}
--- lib/changeTitle.ts
+++
@@ -1,3 +0,0 @@
-export const slugToTitle = (slug: string) =>
-  slug.replace('-', ' ').replace('.mdx', '')
-export const titleToSlug = (title: string) => title.replace(' ', '-')
